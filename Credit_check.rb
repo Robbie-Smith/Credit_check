b07cf5c5b346b52f1194d2431d9f8c1c838b6a4c
Robert Smith <robert.smith160@icloud.com>
2016-10-06 12:20:08 -0600

    Credit_check, aka the luhn algorithm

---
+++ Credit_check.rb
@@ -0,0 +1,42 @@
+require 'pry'
+
+class Credit
+
+  def initialize(card_number)
+    card_s_to_i(card_number)
+  end
+
+  def card_s_to_i(card_number)
+    card_split = card_number.chars.map {|num| num.to_i}
+    double_every_other_digit(card_split)
+  end
+
+  def double_every_other_digit(card_split)
+    card_split.map!.with_index do |value, index|
+      index.even? ? value * 2 : value
+    end
+    add(card_split)
+  end
+
+  def add(card_split)
+    binding.pry
+    card_split.map!{|num| num > 9 ? split_num_and_sum(num) : num}
+    card_split = card_split.reduce(:+) % 10
+    valid(card_split)
+  end
+
+  def split_num_and_sum(num)
+    add = num.to_s.chars.map { |digit| digit.to_i }
+    add.reduce(:+)
+  end
+
+  def valid(card_split)
+    if card_split.eql?(0)
+      puts "The number is valid!"
+    else
+      puts "The number is invalid!"
+    end
+  end
+end
+
+Credit.new('4929735477250543')
